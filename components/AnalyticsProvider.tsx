3d54ddc3e32299466da94487069214d5ca0245d5
AlexSV97 <alejandro97sv@gmail.com>
2026-04-21 12:17:21 +0200

    temp: disable analytics again

--- components/AnalyticsProvider.tsx
+++ components/AnalyticsProvider.tsx
@@ -1,21 +1,8 @@
 'use client';
 
-import { ReactNode, useEffect, useState } from 'react';
-import { useAnalytics, useScrollTracking } from './Analytics';
+import { ReactNode } from 'react';
 
+// Analytics deshabilitado temporalmente
 export function AnalyticsProvider({ children }: { children: ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (mounted) {
-      useAnalytics();
-      useScrollTracking();
-    }
-  }, [mounted]);
-
   return <>{children}</>;
 }
